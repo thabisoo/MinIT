a8e868cb6ec7cbbe5fc8a8990509328a7b26ca6d
Thabiso-Motsoko <thabiso@immedia.co.za>
2019-06-07 13:15:43 +0200

    default records

---
+++ MinIT.Data/Migrations/20190607105017_AddDefaultRecords.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MinIT.Data.Migrations
+{
+    public partial class AddDefaultRecords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO MeetingTypes(Id, Name, IsDeleted, CreatedAt, UpdatedAt) VALUES(newid(), 'MANCO', 'false', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET())");
+            migrationBuilder.Sql("INSERT INTO MeetingItemStatuses(Id, Name, IsDeleted, CreatedAt, UpdatedAt) VALUES(newId(), 'Open', 'false', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
